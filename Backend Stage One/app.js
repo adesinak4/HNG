09ce9721db448319359c001495b1fdb77685e585
Oluwakayode Adesina <adesinak4@gmail.com>
2023-09-08 18:55:16 +0100

    update

--- Backend Stage One/app.js
+++ Backend Stage One/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5050;
 
-const apiRoutes = require('./routes/apiRoutes');
-app.use('/', apiRoutes);
+const Routes = require('./routes/routes');
+app.use('/api', Routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- Backend Stage One/controllers/Controller.js
+++ Backend Stage One/controllers/Controller.js
@@ -4,15 +4,15 @@ const getData = (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
   const currentDay = moment().format('dddd');
-  const currentUTCTime = moment().utc().format();
+  const currentUTCTime = moment().format();
 
   const jsonResponse = {
     slack_name: slackName,
     current_day: currentDay,
     utc_time: currentUTCTime,
     track: track,
-    github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext',
-    github_repo_url: 'https://github.com/username/repo',
+    github_file_url: 'https://github.com/adesinak4/HNG/blob/main/Backend%20Stage%20One/app.js',
+    github_repo_url: 'https://github.com/adesinak4/HNG',
     status_code: 200,
   };
 
