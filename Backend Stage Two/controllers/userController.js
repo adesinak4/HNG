ca000de6558320fae863805a6966e0f3d5d37deb
Oluwakayode Adesina <adesinak4@gmail.com>
2023-09-12 16:06:29 +0100

    Update userController.js

--- Backend Stage Two/controllers/userController.js
+++ Backend Stage Two/controllers/userController.js
@@ -13,7 +13,8 @@ exports.createUser = [
         }
         try {
             const user = await User.create(req.body);
-            res.status(201).json(user);
+            const { user_id, name } = user;
+            res.status(201).json({ user_id, name });
         } catch (error) {
             res.status(500).json({ error: 'Unable to create a user.' });
         }
@@ -42,7 +43,8 @@ exports.updateUser = async (req, res) => {
         if (!user) {
             return res.status(404).json({ error: 'User not found.' });
         }
-        res.status(200).json(user);
+        const { user_id, name } = user;
+        res.status(201).json({ user_id, name });
     } catch (error) {
         res.status(500).json({ error: 'Unable to update the user.' });
     }
